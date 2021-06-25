0dfd845bfc3d97759dc8845eea71083dda01c04b
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-06-25 10:09:42 +0500

    Update scripts.js

--- Fundamentals Part 1/Data Types/scripts.js
+++ Fundamentals Part 1/Data Types/scripts.js
@@ -1,1 +1,25 @@
 
+let js = "amazing";
+if (js === "amazing") alert("Js is a Fun!!! ");
+
+
+
+// Data types and let, const, var differences
+
+let javascriptIsFun = true;
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+javascriptIsFun = 'Yes';
+
+console.log(typeof javascriptIsFun);
+console.log(javascriptIsFun);
+
+let year;
+console.log(typeof year);
+console.log(year);
+year = 1991;
+console.log(typeof year);
+console.log(year);
+console.log(typeof null);
+console.log(typeof undefined);
