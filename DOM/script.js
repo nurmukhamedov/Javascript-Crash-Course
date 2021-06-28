d1120a79bc745d25e97a1f4da376b83efb9d3214
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-06-28 16:04:58 +0500

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value);
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number';
+    }
+
 })
