5d965273aa873be67835524cab8bec47e5dd94ef
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-05 15:36:13 +0500

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -1,4 +1,4 @@
-'use strict';
+ 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 // document.querySelector('.number').textContent = secretNumber;
@@ -7,45 +7,55 @@ const message = document.querySelector('.message');
 const againBtn = document.querySelector('.again');
 const scoreSpan = document.querySelector('.score');
 const number = document.querySelector('.number');
+let highScore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
     if (!guess) {
-        message.textContent = 'No Number';
+        displayMessage('No Number');
     } else if (guess === secretNumber) {
-        message.textContent = 'Congrats You win';
+        displayMessage('Congrats You win');
+        number.textContent = guess;
         document.querySelector('body').style.background = "#60b347";
         number.style.width = "30rem";
-
-
+        if (score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
     } else if (guess > secretNumber) {
         if (score > 1) {
-            message.textContent = 'Your number is too high';
+            displayMessage('Your number is too high');
             score--;
             scoreSpan.textContent = score;
         } else {
-            message.textContent = 'You lost the Game';
+            displayMessage('You lost the Game');
             score--;
             scoreSpan.textContent = 0;
         }
     } else if (guess < secretNumber) {
 
         if (score > 1) {
-            message.textContent = 'Your number is too low';
+            displayMessage('Your number is too low');
             score--;
             scoreSpan.textContent = score;
         } else {
-            message.textContent = 'You lost the Game';
+            displayMessage('You lost the Game');
             score--;
             scoreSpan.textContent = 0;
         }
     }
 });
 againBtn.addEventListener('click', function() {
+    score = 20;
     scoreSpan.textContent = 20;
-    message.textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
+    number.textContent = '';
     const guess = Number(document.querySelector('.guess').value = '');
     document.querySelector('body').style.background = "#222";
-    score = 20;
-})
+    number.style.width = "15rem";
+
+});
