103dc5f31ca5924d643dfd2bcdc2569c0c4012fc
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-05 17:54:38 +0500

    Update script.js

--- DOM/Pig Game/script.js
+++ DOM/Pig Game/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+// selecting elements
+
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
+
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('.hidden');
+
+btnRoll.addEventListener('click', function() {
+
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.classList.remove('.hidden');
+    diceEl.src = `dice-${dice}.png`;
+    if (dice !== 1) {
+        currentScore += dice;
+
+        current0El.textContent = currentScore;
+    }
+
+})
