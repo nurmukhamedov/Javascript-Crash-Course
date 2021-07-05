813bbd148082a3bb72c60922923311f5782b206a
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-05 18:32:18 +0500

    Update script.js

--- DOM/Pig Game/script.js
+++ DOM/Pig Game/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
 // selecting elements
-
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -11,6 +12,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 let currentScore = 0;
+let activePlayer = 0;
 
 
 score0El.textContent = 0;
@@ -25,8 +27,19 @@ btnRoll.addEventListener('click', function() {
     diceEl.src = `dice-${dice}.png`;
     if (dice !== 1) {
         currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        // if (activePlayer === 0) {
+        //     activePlayer = 1;
+        // } else {
+        //     activePlayer = 0;
+        // }
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
 
-        current0El.textContent = currentScore;
     }
 
-})
+});
