60434db38b9d06e033d246505c180da8a9d62957
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-05 16:50:23 +0500

    Update scripts.js

--- DOM/Modal Game/scripts.js
+++ DOM/Modal Game/scripts.js
@@ -1,18 +1,22 @@
-'use strict';
-const modal = document.querySelector('.modal');
-const overlay = document.querySelector('.overlay');
+ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-
-for (let i = 0; i < btnOpenModal.length; i++)
-    btnOpenModal[i].addEventListener('click', function() {
-        modal.classList.remove('hidden'); // we are not selecting class so we don't need to use dot before hidden
-        overlay.classList.remove('hidden');
-    });
-
 const closeFunction = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
 }
+const openFunction = function() {
+    modal.classList.remove('hidden'); // we are not selecting class so we don't need to use dot before hidden
+    overlay.classList.remove('hidden');
+}
+for (let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', openFunction);
 btnCloseModal.addEventListener('click', closeFunction);
 overlay.addEventListener('click', closeFunction);
+
+document.addEventListener('keydown', function(e) {
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeFunction();
+    }
+});
