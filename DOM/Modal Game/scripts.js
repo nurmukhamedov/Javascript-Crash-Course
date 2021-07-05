845c88a0f454ea0b950b4b1b056ee0e4b6702f01
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-05 16:16:42 +0500

    Update scripts.js

--- DOM/Modal Game/scripts.js
+++ DOM/Modal Game/scripts.js
@@ -4,6 +4,15 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-    console.log(btnOpenModal[i].textContent);
+for (let i = 0; i < btnOpenModal.length; i++)
+    btnOpenModal[i].addEventListener('click', function() {
+        modal.classList.remove('hidden'); // we are not selecting class so we don't need to use dot before hidden
+        overlay.classList.remove('hidden');
+    });
+
+const closeFunction = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 }
+btnCloseModal.addEventListener('click', closeFunction);
+overlay.addEventListener('click', closeFunction);
