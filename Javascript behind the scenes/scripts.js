9e7738f3d3c5342193bb442a47656b08338c6cec
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-08 18:49:26 +0500

    Update scripts.js

--- Javascript behind the scenes/scripts.js
+++ Javascript behind the scenes/scripts.js
@@ -22,4 +22,18 @@ function addDecl(a, b) {
 const addExp = function(a, b) {
     return a + b;
 }
-const addArr = (a, b) => a + b;
+const addArr = (a, b) => a + b; 
+
+const jonas = {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function() {
+        console.log(this);
+        console.log(2037 - this.year);
+    },
+    greet: () => {
+        console.log(`Hey ${this.firstName}`);
+    }
+}
+jonas.calcAge();
+jonas.greet(); // Result undefined because arrow function doesn't have its own function
