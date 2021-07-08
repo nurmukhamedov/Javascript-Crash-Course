929968bc8763b94c6d8ccd4d0e0045eb08155065
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-08 17:25:32 +0500

    Update scripts.js

--- Javascript behind the scenes/scripts.js
+++ Javascript behind the scenes/scripts.js
@@ -1,1 +1,25 @@
+'use strict';
 
+// Variable
+
+console.log(myName);
+//console.log(lastName);
+//console.log(job);
+
+var myName = 'Muhammadrasul';
+const lastName = 'Nurmukhamedov';
+let job = 'developer';
+
+// Function
+console.log(addDecl(4, 5));
+//console.log(addExp(4, 5));
+console.log(addArr(4, 5));
+
+function addDecl(a, b) {
+    return a + b;
+}
+
+const addExp = function(a, b) {
+    return a + b;
+}
+const addArr = (a, b) => a + b;
