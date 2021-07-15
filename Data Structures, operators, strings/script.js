643d34ffb09778e288e0aa2af56341aa96c81c18
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-15 21:09:17 +0500

    Update script.js

--- Data Structures, operators, strings/script.js
+++ Data Structures, operators, strings/script.js
@@ -1,46 +1,11 @@
- 'use strict';
+  'use strict';
 
 // // Data needed for a later exercise
 // const flights =
 //     '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
-// // Data needed for first part of the section
-// const restaurant = {
-//     name: 'Classico Italiano',
-//     location: 'Via Angelo Tavanti 23, Firenze, Italy',
-//     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-//     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-//     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-//     order: function(starterIndex, mainIndex) {
-//         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-//     },
+// Data needed for first part of the section
 
-//     orderDelivery: function({ starterIndex, mainIndex, time, address }) {
-//         console.log(`Order recieved ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered ${address} at ${time}`);
-//     },
-//     orderPasta: function(ing1, ing2, ing3) {
-//         console.log(`Here is your delicipus pasta with ${ing1}, ${ing2}, ${ing3}`);
-//     },
-//     orderPizza: function(mainIng, ...otherIng) {
-//         console.log(mainIng);
-//         console.log(otherIng);
-//     },
-
-//     openingHours: {
-//         thu: {
-//             open: 12,
-//             close: 22,
-//         },
-//         fri: {
-//             open: 11,
-//             close: 23,
-//         },
-//         sat: {
-//             open: 0, // Open 24 hours
-//             close: 24,
-//         },
-//     },
-// };
 
 // restaurant.orderPizza('mushrooms', 'ketshup', 'olivia', 'mayonez')
 //     // Spread operator beacue right side of =
@@ -66,16 +31,12 @@
 // add(5, 4, 9, 8, 7);
 // const x = [5, 4, 3, 2, 20, 14];
 // add(...x);
-
-// // const ingredients = [prompt("let/'s choose ingredients1"), prompt("choose ingredients2"), prompt("choose ingredients3")];
-// // console.log(restaurant.orderPasta(...ingredients));
-
-// // restaurant.orderDelivery({
-// //     time: '22:30',
-// //     address: 'Via Del sole',
-// //     starterIndex: 3,
-// //     mainIndex: 2
-// // });
+// restaurant.orderDelivery({
+//     time: '22:30',
+//     address: 'Via Del sole',
+//     starterIndex: 3,
+//     mainIndex: 2
+// });
 
 // // const newArray = [...restaurant.mainMenu, 'Gnocci']
 // // console.log(newArray);
@@ -164,19 +125,204 @@
 // // short circuiting 
 // console.log(3 || 'Jonas');
 // console.log(undefined || 0);
-// console.log(null || 23); 
+// console.log(null || 23);
 
-let firstName = 'Jonas';
-let oldFirstName = firstName;
-firstName = 'Milana';
-console.log(firstName, oldFirstName);
+// const game = {
+//     team1: 'Bayern Munich',
+//     team2: 'Borrussia Dortmund',
+//     players: [
+//         [
+//             'Neuer',
+//             'Pavard',
+//             'Martinez',
+//             'Alaba',
+//             'Davies',
+//             'Kimmich',
+//             'Goretzka',
+//             'Coman',
+//             'Muller',
+//             'Gnarby',
+//             'Lewandowski',
+//         ],
+//         [
+//             'Burki',
+//             'Schulz',
+//             'Hummels',
+//             'Akanji',
+//             'Hakimi',
+//             'Weigl',
+//             'Witsel',
+//             'Hazard',
+//             'Brandt',
+//             'Sancho',
+//             'Gotze',
+//         ],
+//     ],
+//     score: '4:0',
+//     scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//     date: 'Nov 9th, 2037',
+//     odds: {
+//         team1: 1.33,
+//         x: 3.25,
+//         team2: 6.5,
+//     },
 
-const jessica = {
-    name: 'David',
-    age: 42
+//     printGoals: function({ lewandowski, muller, davies, kimmich }) {
+//         console.log(`Lewandowki socred ${lewandowski} goals and Muller played on ${muller}, Davies playes on${davies}, Kimmich played on ${kimmich}`);
+//     }
+// };
+
+// const [player1, player2] = game.players;
+// console.log(player1, player2);
+
+// const [gk, ...fieldPlayers] = player1;
+// console.log(gk, fieldPlayers);
+// const [gk2, ...fieldPlayers2] = player2;
+// console.log(gk2, fieldPlayers2);
+
+// const allPlayers = [...player1, ...player2];
+// console.log(allPlayers);
+
+// const players1final = [...player1, 'Thiago', 'Countinho', 'Perisic'];
+// console.log(players1final);
+
+// const { team1, x: draw, team2 } = game.odds;
+// console.log(team1, draw, team2);
+
+// game.printGoals({
+//     lewandowski: 2,
+//     muller: 20,
+//     davies: 17,
+//     kimmich: 6
+// })
+
+// let firstName = 'Jonas';
+// let oldFirstName = firstName;
+// firstName = 'Milana';
+// console.log(firstName, oldFirstName);
+
+// const jessica = {
+//     name: 'David',
+//     age: 42
+// }
+// const marriedJessica = jessica;
+
+// marriedJessica.age = 56;
+
+// console.log(marriedJessica, jessica);  
+
+'use strict';
+
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+    [weekdays[3]]: {
+        open: 12,
+        close: 22,
+    },
+    [weekdays[4]]: {
+        open: 11,
+        close: 23,
+    },
+    [weekdays[5]]: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    // ES6 enhanced object literals
+    openingHours,
+
+    order(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    },
+
+    orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+        console.log(
+            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+        );
+    },
+
+    orderPasta(ing1, ing2, ing3) {
+        console.log(
+            `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+        );
+    },
+
+    orderPizza(mainIngredient, ...otherIngredients) {
+        console.log(mainIngredient);
+        console.log(otherIngredients);
+    },
+};
+
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+    const open = restaurant.openingHours[day];
+    console.log(`On ${day} we open at ${open}`);
 }
-const marriedJessica = jessica;
 
-marriedJessica.age = 56;
+const properties = Object.keys(openingHours);
+console.log(properties);
 
-console.log(marriedJessica, jessica);
+let openStr = `We are open on ${ properties.length } days: `;
+for (const day of properties) {
+    openStr += ` ${ day } `;
+}
+console.log(openStr);
+
+const values = Object.values(openingHours);
+
+const entries = Object.entries(openingHours);
+
+for (const [day, { open, close }] of entries) {
+    console.log(` On ${day} we open at ${open} and close at ${close}`);
+}
+
+// console.log(restaurant.order ? .(1, 0) ? ? 'Method not found');
+
+
+// const make1 = prompt('what is your favorite laptop company');
+// const model1 = prompt('what is your favorite laptop type');
+// const year1 = prompt('Year');
+// const getLaptop = function(make, model, year) {
+//     return {
+//         make: make,
+//         model: model,
+//         year: year
+//     }
+// }
+
+// console.log(getLaptop(make1, model1, year1));
+
+// function getLaptop2(make, model, year) {
+//     return {
+//         sayModel: function() {
+//             return make;
+//         }
+//     }
+// }
+// console.log(getLaptop2("Apple", "MacBook", "2015").sayModel()); 
+
+// const make1 = prompt('what is your favorite laptop company');
+// const model1 = prompt('what is your favorite laptop type');
+// const year1 = prompt('Year');
+// const getLaptop = function(make, model, year) {
+//     return {
+//         make,
+//         model,
+//         year,
+//     }
+// }
+// console.log(getLaptop(make1, model1, year1));
