004b3b430d6b66c282b639838a9a59677750d585
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-22 16:43:33 +0500

    Update scripts.js

--- working with arrays/scripts.js
+++ working with arrays/scripts.js
@@ -14,4 +14,28 @@ movements.forEach(function(movement) {
         } else {
             console.log(`You withdrawed ${movement}`);
         }
-    })
+    }); 
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+const movementUsd = movements.map(function(mov) {
+    return mov * euroToUsd;
+});
+console.log(movements);
+console.log(movementUsd);
+
+const movementToUsdFor = [];
+
+for (const mov of movements) movementToUsdFor.push(mov * euroToUsd);
+console.log(movementToUsdFor);
+
+const movementDescription = movements.map(function(mov, i, arr) {
+    if (mov > 0) {
+        return `Movement ${i +1}:  You deposited ${mov}  `;
+    } else {
+        return `You withdrawed ${i + 1}: ${mov} `;
+    }
+});
+console.log(...movementDescription);
