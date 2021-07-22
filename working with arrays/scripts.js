68e549dbad52603be11d2a2a1e3b31bc1f5ee53b
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-22 16:03:53 +0500

    Create scripts.js

---
+++ working with arrays/scripts.js
@@ -0,0 +1,17 @@
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for (const movement of movements) {
+    if (movement > 0) {
+        console.log(`You deposited ${movement}`);
+    } else {
+        console.log(`You withdrawed ${movement}`);
+    }
+}
+console.log('----------------ForEach-----------------');
+
+movements.forEach(function(movement) {
+        if (movement > 0) {
+            console.log(`You deposited ${movement}`);
+        } else {
+            console.log(`You withdrawed ${movement}`);
+        }
+    })
