d18918d19e3182b0291b6d4804be253957971055
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-27 14:36:41 +0500

    Update script.js

--- OOP/ES6 Class/script.js
+++ OOP/ES6 Class/script.js
@@ -11,4 +11,22 @@ class PersonCl {
 const jessica = new PersonCl('Jessica', 1998);
 const brown = new PersonCl('Brown', 1995);
 jessica.calcAge();
-brown.calcAge();
+brown.calcAge();  
+
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+    get carAcc() {
+        this.speed += 10;
+        console.log(`${this.make} is going to at ${this.speed}`);
+    }
+    carbrake = function() {
+        this.speed -= 10;
+        console.log(`${this.make} is going to at ${this.speed}`);
+    }
+}
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+bmw.carAcc;
