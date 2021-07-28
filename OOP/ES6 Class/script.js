35b6de89b3826d471c8cd3f6485368357721f59b
Abdurasul Nurmukhamedov <66766636+nurmukhamedov@users.noreply.github.com>
2021-07-28 16:07:50 +0500

    Update script.js

--- OOP/ES6 Class/script.js
+++ OOP/ES6 Class/script.js
@@ -30,3 +30,55 @@ class Car {
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes', 95);
 bmw.carAcc;
+// Inheritance between contructor function
+const Person = function(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+}
+Person.prototype.calcAge = function() {
+    console.log(2037 - this.birthYear);
+}
+const Student = function(firstName, birthYear, course) {
+    Person.call(this, firstName, birthYear);
+    this.course = course;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+const mike = new Student('Dave', 1996, 'Computer Sciense');
+console.log(mike);
+mike.calcAge();
+
+class PersonCl {
+    constructor(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+    calcAge() {
+        console.log(` ${this.firstName} is ${2037 -this.birthYear} years Old`);
+    }
+}
+
+class StudentCl extends PersonCl {
+    constructor(firstName, birthYear, course) {
+        super(firstName, birthYear); // Always needs to happen first
+        this.course = course;
+    }
+    introduce() {
+        console.log(`My name is ${this.firstName} and I study ${this.course}`);
+    }
+}
+const martha = new StudentCl('Martha', 2002, 'Computer Sciense');
+console.log(martha);
+martha.introduce();
+martha.calcAge();
+
+class Account {
+    constructor(owner, currency, pin) {
+        this.owner = owner;
+        this.currency = currency;
+        this.pin = pin;
+    }
+}
+const acc1 = new Account('David', 'EUR', 1111);
+console.log(acc1);
